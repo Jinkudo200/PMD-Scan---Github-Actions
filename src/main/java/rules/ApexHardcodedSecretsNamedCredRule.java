d4b23d7ec4e3a45e354a9edd0c70c7b2059b298c
Ayman EL KIHEL <91762035+Jinkudo200@users.noreply.github.com>
2025-09-19 10:47:25 +0100

    Update ApexHardcodedSecretsNamedCredRule.java

--- src/main/java/rules/ApexHardcodedSecretsNamedCredRule.java
+++ src/main/java/rules/ApexHardcodedSecretsNamedCredRule.java
@@ -5,61 +5,44 @@
 package rules;
 
 import net.sourceforge.pmd.lang.apex.ast.ASTMethodCallExpression;
-import net.sourceforge.pmd.lang.apex.ast.ASTVariableExpression;
-import net.sourceforge.pmd.lang.apex.ast.ASTBinaryExpression;
 import net.sourceforge.pmd.lang.apex.ast.ASTUserClass;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
+import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
 import net.sourceforge.pmd.lang.apex.rule.internal.Helper;
-import org.checkerframework.checker.nullness.qual.NonNull;
-
-import java.util.List;
 
 public class ApexHardcodedSecretsNamedCredRule extends AbstractApexRule {
 
     public ApexHardcodedSecretsNamedCredRule() {
         setName("ApexHardcodedSecretsNamedCredRule");
-        setDescription("Detect hardcoded secrets that should use Named Credentials");
-        setMessage("Avoid hardcoding secrets, use Named Credentials instead.");
+        setMessage("Avoid hardcoding secrets; use Named Credentials instead.");
         setPriority(net.sourceforge.pmd.lang.rule.RulePriority.HIGH);
     }
 
     @Override
-    protected @NonNull net.sourceforge.pmd.lang.rule.RuleTargetSelector buildTargetSelector() {
-        return net.sourceforge.pmd.lang.rule.RuleTargetSelector.forTypes(ASTUserClass.class);
+    protected RuleTargetSelector buildTargetSelector() {
+        return RuleTargetSelector.forTypes(ASTUserClass.class);
     }
 
     @Override
     public Object visit(ASTUserClass node, Object data) {
 
-        // Skip test classes
-        if (Helper.isTestMethodOrClass(node) || Helper.isSystemLevelClass(node)) {
-            return data;
-        }
+        for (ASTMethodCallExpression call : node.descendants(ASTMethodCallExpression.class)) {
 
-        // Find all method calls in the class
-        for (ASTMethodCallExpression call : node.descendants(ASTMethodCallExpression.class).toList()) {
-
+            // Only analyze certain method calls like HTTP request setEndpoint or setHeader
             String fullMethodName = call.getFullMethodName();
+            if ("HTTPRequest.setEndpoint".equals(fullMethodName) ||
+                "HTTPRequest.setHeader".equals(fullMethodName)) {
 
-            // Detect problematic methods
-            if ("HttpRequest.setHeader".equalsIgnoreCase(fullMethodName) ||
-                "HttpRequest.setEndpoint".equalsIgnoreCase(fullMethodName)) {
-
-                // Check for string literals or variables
-                List<ASTVariableExpression> args = call.descendants(ASTVariableExpression.class).toList();
-
-                for (ASTVariableExpression arg : args) {
-                    String argName = arg.getImage();
-                    if (argName != null && !argName.isEmpty()) {
-                        // Flag the variable
-                        asCtx(data).addViolation(arg);
-                    }
-                }
-
-                // Also detect binary expressions in arguments (concatenated strings)
-                if (!call.descendants(ASTBinaryExpression.class).toList().isEmpty()) {
+                // Check for literals (hardcoded secrets)
+                if (call.firstChildOrNull(net.sourceforge.pmd.lang.apex.ast.ASTLiteralExpression.class) != null) {
                     asCtx(data).addViolation(call);
                 }
+
+                // Check for binary expressions (concatenated strings)
+                else if (call.firstChildOrNull(net.sourceforge.pmd.lang.apex.ast.ASTBinaryExpression.class) != null) {
+                    asCtx(data).addViolation(call);
+                }
+
             }
         }
 
