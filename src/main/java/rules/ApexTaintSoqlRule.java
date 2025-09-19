6b827d5d83a0501e539545f7dcb01835e7205069
Ayman EL KIHEL <91762035+Jinkudo200@users.noreply.github.com>
2025-09-19 10:07:11 +0100

    Update ApexTaintSoqlRule.java

--- src/main/java/rules/ApexTaintSoqlRule.java
+++ src/main/java/rules/ApexTaintSoqlRule.java
@@ -3,69 +3,45 @@ package rules;
 import java.util.HashSet;
 import java.util.Set;
 
-import net.sourceforge.pmd.lang.apex.ast.ASTAssignmentExpression;
-import net.sourceforge.pmd.lang.apex.ast.ASTMethod;
-import net.sourceforge.pmd.lang.apex.ast.ASTModifierList;
-import net.sourceforge.pmd.lang.apex.ast.ASTParameter;
-import net.sourceforge.pmd.lang.apex.ast.ASTVariableDeclaration;
-import net.sourceforge.pmd.lang.apex.ast.ASTVariableExpression;
-import net.sourceforge.pmd.lang.apex.ast.ApexNode;
+import net.sourceforge.pmd.lang.apex.ast.*;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
 import net.sourceforge.pmd.lang.apex.rule.internal.Helper;
+import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
 
-/**
- * Custom PMD rule to detect unsafe variables in SOQL queries (SQL injection risk)
- * Priority: 2 (High)
- */
 public class ApexTaintSoqlRule extends AbstractApexRule {
 
-    private final Set<String> taintedVars = new HashSet<>();
+    private final Set<String> safeVars = new HashSet<>();
+
+    public ApexTaintSoqlRule() {
+        setPriority(2);
+    }
 
     @Override
-    public Object visit(ASTMethod node, Object data) {
+    protected RuleTargetSelector buildTargetSelector() {
+        return RuleTargetSelector.forTypes(ASTUserClass.class);
+    }
 
-        // Mark parameters of public or annotated methods as safe
-        for (ASTParameter p : node.findChildrenOfType(ASTParameter.class)) {
+    @Override
+    public Object visit(ASTUserClass node, Object data) {
 
-            ASTModifierList mods = node.getFirstChild(ASTModifierList.class);
-            boolean isPublic = mods != null && mods.isPublic();
-            boolean hasAnnotation = !node.findChildrenOfType(net.sourceforge.pmd.lang.apex.ast.ASTAnnotation.class).isEmpty();
+        for (ASTMethod m : node.descendants(ASTMethod.class)) {
 
-            if (isPublic || hasAnnotation) {
-                taintedVars.add(Helper.getFQVariableName(p));
+            for (ASTParameter p : m.children(ASTParameter.class)) {
+                safeVars.add(Helper.getFQVariableName(p));
+            }
+
+            for (ASTMethodCallExpression call : m.descendants(ASTMethodCallExpression.class)) {
+                if (call.getMethodName() != null && call.getMethodName().equalsIgnoreCase("Database.query")) {
+                    for (ASTVariableExpression var : call.descendants(ASTVariableExpression.class).toList()) {
+                        String fq = Helper.getFQVariableName(var);
+                        if (!safeVars.contains(fq)) {
+                            asCtx(data).addViolation(var);
+                        }
+                    }
+                }
             }
         }
 
-        return super.visit(node, data);
-    }
-
-    @Override
-    public Object visit(ASTVariableDeclaration node, Object data) {
-        checkAssignment(node);
-        return super.visit(node, data);
-    }
-
-    @Override
-    public Object visit(ASTAssignmentExpression node, Object data) {
-        checkAssignment(node);
-        return super.visit(node, data);
-    }
-
-    private void checkAssignment(ApexNode<?> node) {
-        ASTVariableExpression leftVar = node.firstChild(ASTVariableExpression.class);
-        ASTVariableExpression rightVar = null;
-
-        // get first ASTVariableExpression child from descendants
-        for (ASTVariableExpression v : node.descendants(ASTVariableExpression.class)) {
-            rightVar = v;
-            break;
-        }
-
-        if (leftVar != null && rightVar != null) {
-            String fqName = Helper.getFQVariableName(leftVar);
-            if (taintedVars.contains(Helper.getFQVariableName(rightVar))) {
-                taintedVars.add(fqName);
-            }
-        }
+        return data;
     }
 }
